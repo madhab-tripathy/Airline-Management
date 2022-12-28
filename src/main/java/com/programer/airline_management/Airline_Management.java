c608e64b0caa7e347e5b7ba1c59f4051cc77a61f
Madhab Tripathy <madhabtripathy1349@gmail.com>
2022-12-28 13:53:36 +0530

    Complete Project

---
+++ src/main/java/com/programer/airline_management/Airline_Management.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.programer.airline_management;
+
+/**
+ *
+ * @author ROG
+ */
+public class Airline_Management {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
